0b59fb9d09fdf99b25a15cf34ffa4f645eb448c0
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-10-29 22:43:10 +0100

    Update ipfw-workstation.sh

--- ipfw-workstation.sh
+++ ipfw-workstation.sh
@@ -10,3 +10,6 @@ sysrc firewall_allowservices="any"
 # To enable services like remote SSH access or setting up a web server
 # uncommenting the following up will allow them when issuing this script.
 # sysrc firewall_myservices="22/tcp 80/tcp 443/tcp"
+
+## References:
+## https://www.adminbyaccident.com/freebsd/how-to-freebsd/how-to-setup-a-simple-firewall-in-freebsd-using-ipfw/
