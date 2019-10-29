968b189d5a265c41e46ab0d3596bdaa2feb3f1e6
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-10-29 22:41:48 +0100

    Create ipfw-workstation.sh

---
+++ ipfw-workstation.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Simple IPFW workstation firewall
+sysrc firewall_enable="YES"
+sysrc firewall_quiet="YES"
+sysrc firewall_type="workstation"
+sysrc firewall_logdeny="YES"
+sysrc firewall_allowservices="any"
+
+# To enable services like remote SSH access or setting up a web server
+# uncommenting the following up will allow them when issuing this script.
+# sysrc firewall_myservices="22/tcp 80/tcp 443/tcp"
