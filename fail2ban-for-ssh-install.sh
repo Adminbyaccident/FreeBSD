f91a2a47abad0be4955de3beaf147fb315948169
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-11-02 21:08:21 +0100

    Create fail2ban-for-ssh-install.sh

---
+++ fail2ban-for-ssh-install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Change the default pkg repository from quarterly to latest
+sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
+
+# Update packages (it will first download the pkg repo from latest)
+# secondly it will upgrade any installed packages.
+pkg upgrade -y
+
+# Install Fail2ban
+pkg install -y py36-fail2ban
+
+# Enable the service to be started/stopped/restarted/etc
+sysrc fail2ban_enable="YES"
+
+# Configure a Jail to protect the SSH connections
+touch /usr/local/etc/fail2ban/jail.d/ssh-ipfw.local
+
+echo "[ssh-ipfw]
+enabled = true
+filter = sshd
+action = ipfw[name=SSH, port=ssh, protocol=tcp]
+#       sendmail-whois[name=SSH, dest=root@localhost, sender=noreply@localhost]
+logpath = /var/log/auth.log
+findtime = 600
+maxretry = 3
+bantime = 604800" >> /usr/local/etc/fail2ban/jail.d/ssh-ipfw.local
+
+# Start the service
+service fail2ban start
