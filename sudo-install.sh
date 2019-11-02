64f80649b515996b1287440595e8349b456e3405
Adminbyaccident <46286613+Adminbyaccident@users.noreply.github.com>
2019-11-02 21:27:41 +0100

    Create sudo-install.sh

---
+++ sudo-install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Change the default pkg repository from quarterly to latest
+sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
+
+# Update packages (it will first download the pkg repo from latest)
+# secondly it will upgrade any installed packages.
+pkg upgrade -y
+
+# Install sudo
+pkg install -y sudo
+
+# Configure sudo so users in the wheel group can elevate privileges
+sed -ip 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/g' /usr/local/etc/sudoers
+
+## References:
+## https://www.adminbyaccident.com/freebsd/how-to-freebsd/install-sudo-freebsd/
