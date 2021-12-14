8bd995762544666418937e1e5ca890f6e6993a14
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2021-12-14 18:41:22 +0100

    Update pkg-latest.sh

--- pkg-latest.sh
+++ pkg-latest.sh
@@ -1,10 +1,31 @@
 #!/bin/sh
-
-# Instructions on how to use this script 
-
+# Instructions on how to use this script:
 # chmod +x SCRIPTNAME.sh
-
 # sudo ./SCRIPTNAME.sh
+#
+# SCRIPT: pkg-latest.sh
+# AUTHOR: ALBERT VALBUENA
+# DATE: 13-10-2019
+# SET FOR: Production
+# (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: FreeBSD 12/13
+#
+# PURPOSE: This script configures the pkg tool to use the latest versions on a FreeBSD system
+#
+# REV LIST:
+# DATE: 14-12-2021
+# BY: ALBERT VALBUENA
+# MODIFICATION: 14-12-2021
+#
+#
+# set -n # Uncomment to check your syntax, without execution.
+# # NOTE: Do not forget to put the comment back in or
+# # the shell script will not execute!
+
+##########################################################
+################ BEGINNING OF MAIN #######################
+##########################################################
 
 # Change the default pkg repository from quarterly to latest
 sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
