05beff55f7ff16e7f8c74b0a497622df30e3758c
Albert Valbuena <46286613+Adminbyaccident@users.noreply.github.com>
2022-09-07 21:18:41 +0200

    Update fail2ban-for-ssh-install.sh
    Updated version

--- fail2ban-for-ssh-install.sh
+++ fail2ban-for-ssh-install.sh
@@ -35,7 +35,7 @@ sed -ip 's/quarterly/latest/g' /etc/pkg/FreeBSD.conf
 pkg upgrade -y
 
 # Install Fail2ban
-pkg install -y py38-fail2ban
+pkg install -y py39-fail2ban
 
 # Enable the service to be started/stopped/restarted/etc
 sysrc fail2ban_enable="YES"
